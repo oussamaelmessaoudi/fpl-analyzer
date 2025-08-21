ee9efc5c4e344adab3158a0fec2c8870b3140ac3
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-08-21 01:12:49 +0100

    Adding the logger to the service layer

--- src/main/java/com/fpl/fplanalyzer/service/FplService.java
+++ src/main/java/com/fpl/fplanalyzer/service/FplService.java
@@ -2,15 +2,20 @@ package com.fpl.fplanalyzer.service;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.fpl.fplanalyzer.client.FplApiClient;
+import com.fpl.fplanalyzer.exception.UserNotFoundException;
 import com.fpl.fplanalyzer.model.FplStats;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Service
 public class FplService {
     private final FplApiClient fplApiClient;
+    private static final Logger logger = LoggerFactory.getLogger(FplService.class);
 
     @Autowired
     public FplService(FplApiClient fplApiClient) {
@@ -18,35 +23,40 @@ public class FplService {
     }
 
 
-    public FplStats getStats(int userId) {
+    public FplStats getStats(int userId)  {
+
         int sumOfRanks=0;
+        logger.info("Fetching summary/history stats for user Id: {}",userId);
         JSONObject summary = fplApiClient.fetchUserSummary(userId);
         JSONObject history = fplApiClient.fetchUserHistory(userId);
+            int currentRank = summary.optInt("summary_overall_rank",-1);
+            String managerName = summary.optString("player_first_name",null)+" "+summary.optString("player_last_name",null);
+            JSONArray seasons = history.optJSONArray("past");
+            int bestSeasonRank = Integer.MAX_VALUE;
+            int seasonsPlayed = 0;
+            String bestSeasonName = "";
 
-        int currentRank = summary.optInt("summary_overall_rank",-1);
-        String managerName = summary.optString("player_first_name",null)+" "+summary.optString("player_last_name",null);
-        JSONArray seasons = history.optJSONArray("past");
-        int bestSeasonRank = Integer.MAX_VALUE;
-        int seasonsPlayed = 0;
-        String bestSeasonName = "";
-
-        if(seasons != null){
-            seasonsPlayed = seasons.length();
-            for(int i = 0;i < seasons.length();i++){
-                JSONObject season = seasons.optJSONObject(i);
-                int rank = season.optInt("rank");
-                sumOfRanks += rank;
-                if(rank < bestSeasonRank){
-                    bestSeasonRank = rank;
-                    bestSeasonName = season.optString("season_name");
+            if(seasons != null){
+                seasonsPlayed = seasons.length();
+                for(int i = 0;i < seasons.length();i++){
+                    JSONObject season = seasons.optJSONObject(i);
+                    int rank = season.optInt("rank");
+                    sumOfRanks += rank;
+                    if(rank < bestSeasonRank){
+                        bestSeasonRank = rank;
+                        bestSeasonName = season.optString("season_name");
+                    }
                 }
             }
-        }
-        if(bestSeasonRank == Integer.MAX_VALUE){
-            bestSeasonRank = -1;
-            bestSeasonName = "Not Found";
-        }
+            if(bestSeasonRank == Integer.MAX_VALUE){
+                bestSeasonRank = -1;
+                bestSeasonName = "Not Found";
+            }
+            FplStats stats = new FplStats(managerName,currentRank,bestSeasonRank,seasonsPlayed,bestSeasonName,(double) sumOfRanks/seasonsPlayed);
+        logger.debug("Parsed stats {}",stats);
 
-        return new FplStats(managerName,currentRank,bestSeasonRank,seasonsPlayed,bestSeasonName,(double) sumOfRanks/seasonsPlayed);
+        return stats;
+
+
     }
 }
