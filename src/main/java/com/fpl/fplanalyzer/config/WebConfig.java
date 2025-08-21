2b36df5479433b8040e246a685a1bc551df4545b
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-08-21 14:09:43 +0100

    making the service cacheable

--- src/main/java/com/fpl/fplanalyzer/config/WebConfig.java
+++ src/main/java/com/fpl/fplanalyzer/config/WebConfig.java
@@ -1,6 +1,17 @@
 package com.fpl.fplanalyzer.config;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer{
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/fpl/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET")
+                .allowedMethods("*");
+    }
 }
